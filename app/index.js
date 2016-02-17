b9569cc72f4d293da73352af8288fbcfa6dbe108
Pennings <baspster@gmail.com>
2016-02-17 10:57:02 +0100

    Dependencies for package.json

--- app/index.js
+++ app/index.js
@@ -66,7 +66,30 @@ module.exports = generators.Base.extend({
     'package.json': function() {
       this.fs.writeJSON(this.destinationPath('package.json'), {
         name: appname,
-        private: true
+        version: '0.1.0',
+        private: true,
+        scripts: {
+          test: 'echo \"Error: no test specified\" && exit 1'
+        },
+        dependencies: {
+          'angular2': '^2.0.0-beta.6',
+          'bootstrap': '^3.3.6',
+          'es6-promise': '^3.0.2',
+          'es6-shim': '^0.33.3',
+          'jquery': '^2.2.0',
+          'moment': '^2.11.2',
+          'reflect-metadata': '0.1.2',
+          'rxjs': '5.0.0-beta.0',
+          'systemjs': '^0.19.21',
+          'zone.js': '0.5.14'
+        },
+        devDependencies: {
+          'browser-sync': '^2.11.1',
+          'connect-history-api-fallback': '^1.1.0',
+          'gulp': '^3.9.1',
+          'gulp-inject': '^3.0.0',
+          'gulp-typescript': '^2.11.0'
+        }
       });
     }
   },
